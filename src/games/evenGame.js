0441a53ab84559c3dae27f42d712e51c8f306781
ApricotLace <AsuraCoi@gmail.com>
2018-10-24 12:49:25 +0300

    evenGames.js moved to a folder games

--- src/games/evenGame.js
+++ src/games/evenGame.js
@@ -1,34 +1,28 @@
 import readlineSync from 'readline-sync';
-import sayHi from './index';
+import sayHi from '..';
 
 const endGame = 3;
-let counter = 0;
 const maxRandNum = 100;
 const minRandNum = 0;
 const isEven = number => number % 2 === 0;
 const generateRandomNum = () => Math.floor(Math.random() * (maxRandNum - minRandNum)) + minRandNum;
-const correctAnswerSetter = (randomNum) => {
-  const answer = isEven(randomNum) ? 'yes' : 'no';
-  return answer;
-};
+const correctAnswerSetter = randomNum => (isEven(randomNum) ? 'yes' : 'no');
 
-console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".');
-const userName = sayHi();
 const execGame = () => {
-  for (counter; counter < endGame; counter += 1) {
-    const randomNum = generateRandomNum();
-    console.log(`Question: ${randomNum}`);
-    const correctAnswer = correctAnswerSetter(randomNum);
+  console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
+  const userName = sayHi();
+  for (let counter = 0; counter < endGame; counter += 1) {
+    const question = generateRandomNum();
+    console.log(`Question: ${question}`);
+    const correctAnswer = correctAnswerSetter(question);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer !== correctAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'\nLet's try again, ${userName}`);
-      break;
+      return;
     }
     console.log('Correct!');
   }
-  if (counter === endGame) {
-    console.log(`Congratulations, ${userName}`);
-  }
+  console.log(`Congratulations, ${userName}`);
 };
 
 export default execGame;
