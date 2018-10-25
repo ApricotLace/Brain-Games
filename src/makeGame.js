ce51bb1eed2b41a987f33bf30faaf2ee4af8c21f
ApricotLace <AsuraCoi@gmail.com>
2018-10-25 14:35:35 +0300

    Delete useless modules

--- src/makeGame.js
+++
@@ -1,8 +0,0 @@
-import {
-  cons,
-} from 'hexlet-pairs';
-
-const makeLogic = (question, correctAnswer) => cons(question, correctAnswer);
-const makeGame = (gameRules, gameLogic) => cons(gameRules, gameLogic);
-
-export { makeLogic, makeGame };
