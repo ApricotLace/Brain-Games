2e96dc6ee0df82c518b2e0b52832547919771d29
ApricotLace <AsuraCoi@gmail.com>
2018-10-26 16:36:46 +0300

    Delete old modules, update utils.js - delete useless make functions

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,2 @@
-import {
-  cons,
-} from 'hexlet-pairs';
-
-export const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-
-export const game = (
-  gameDescription,
-  generateQuestionAnswerPair,
-) => cons(gameDescription, generateQuestionAnswerPair);
-
-export const makeQuestionAnswerPair = (question, correctAnswer) => cons(question, correctAnswer);
+const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+export default getRandomNum;
